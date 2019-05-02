752e7d76dd8b69edb5cca86fcdbd82fdcdba6af3
Brandon Houghton <bhoughton@cmu.edu>
2019-05-01 21:49:29 -0400

    Fixed Multiprossesing bug

--- sample.py
+++ sample.py
@@ -1,17 +1,17 @@
-from minerl.data import DataPipeline
+from minerl import data
 from minerl import env
 
 import os
 
 
 def main():
-    d = DataPipeline(os.path.join('C:', os.sep, 'data', 'data_texture_1_low_res'), 2, 32, 32)
+    d = data.init(os.path.join('C:', os.sep, 'data', 'data_texture_1_low_res'))
     # e = env.init()
     # obs = env.reset()
 
-    for batch in d.batch_iter(64, 64):
+    for batch in d.batch_iter(64, None):
         print("Batch len:", len(batch))
-        return
+
 
     # while not env.isFinished():
     #     action = actionSpace.sample()
