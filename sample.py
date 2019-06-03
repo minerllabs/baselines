b771398ca058781aed63d2d583a60dc64ddc1b78
Brandon Houghton <bhoughton@cmu.edu>
2019-06-03 01:14:23 -0400

    Obtain diamond (#1)
    * Added debug env to test handlers
    
    * Add attack action for validation
    
    * Removed attack
    
    * Improved testing of obtain envs
    
    * Added equip handler, fixed nearbyCraft and nearbySmelt handlers
    Updated quick_start test to demonstrate these features
    
    * Fixed behavior of place command
    
    * Added demonstration of tree-chop reward
    Moved test sample to flat world

--- sample.py
+++ sample.py
@@ -1,10 +1,12 @@
+import time
+
 import minerl
 import itertools
 import gym
 import sys
 import numpy as np
 
-NUM_EPISODES = 42
+NUM_EPISODES = 4
 
 
 def step_data(environment='MineRLTreechop-v0'):
@@ -20,6 +22,80 @@ def step_data(environment='MineRLTreechop-v0'):
         counter += 1
 
 
+def gen_obtain_debug_actions(env):
+    actions = []
+
+    def act(**kwargs):
+        action = env.default_action.copy()
+        for key, value in kwargs.items():
+            action[key] = value
+        actions.append(action)
+
+    act(camera=np.array([45.0, 0.0], dtype=np.float32))
+
+    # Testing craft handlers
+    act(place='log')
+    act(craft='stick')
+    act(craft='stick')  # Should fail - no more planks remaining
+    act(craft='planks')
+    act(craft='crafting_table')
+
+    act(camera=np.array([0.0, 90.0], dtype=np.float32))
+
+    # Testing nearbyCraft implementation (note crafting table must be in view of the agent)
+    act(nearbyCraft='stone_pickaxe')  # Should fail - no crafting table in view
+    act(place='crafting_table')
+    act(nearbyCraft='stone_pickaxe')
+
+    act(camera=np.array([0.0, 90.0], dtype=np.float32))
+
+    # Testing nearbySmelt implementation (note furnace must be in view of the agent)
+    act(nearbySmelt='iron_ingot')  # Should fail - no furnace in view
+    act(place='furnace')
+    act(nearbySmelt='iron_ingot')
+    act(nearbySmelt='iron_ingot')
+    act(nearbySmelt='iron_ingot')
+
+    act(camera=np.array([45.0, 0.0], dtype=np.float32))
+
+    # Test place mechanic (attack ground first to clear grass)
+    act(attack=1)
+    [(act(jump=1), act(jump=1), act(jump=1), act(jump=1), act(jump=1), act(place='cobblestone'), act()) for _ in range(2)]
+    act(equip='stone_pickaxe')
+    [act(attack=1) for _ in range(40)]
+
+    act(camera=np.array([-45.0, -90.0], dtype=np.float32))
+    act(nearbyCraft='stone_axe')
+    act(equip='stone_axe')
+
+    # Test log reward
+    act(camera=np.array([0.0, -90.0], dtype=np.float32))
+    [act(attack=1) for _ in range(40)]
+    [act(forward=1) for _ in range(10)]
+
+
+    return actions
+
+
+def test_env(environment='MineRLObtainTest-v0'):
+    env = gym.make(environment)
+    env.reset()
+    done = False
+
+    for action in gen_obtain_debug_actions(env):
+        obs, reward, done, info = env.step(action)
+        if reward != 0:
+            print(reward)
+        # time.sleep(0.2)
+
+    while not done:
+        obs, reward, done, info = env.step(env.default_action)
+        if reward != 0:
+            print(reward)
+    print("MISSION DONE")
+
+
+
 def step_env(environment='MineRLObtainIronPickaxe-v0'):
     # Run random agent through environment
     env = gym.make(environment) # or try 'MineRLNavigateDense-v0'
@@ -31,12 +107,13 @@ def step_env(environment='MineRLObtainIronPickaxe-v0'):
         while not done:
             # This default action has only been added for MineRLObtainIronPickaxe-v0 so far
             action = env.default_action
-            action['craft'] = env.action_space.spaces['craft'].sample()
-            action['nearbyCraft'] = env.action_space.spaces['nearbyCraft'].sample()
-            action['nearbySmelt'] = env.action_space.spaces['nearbySmelt'].sample()
+            action['forward'] = env.action_space.spaces['forward'].sample()
+            action['attack'] = 1
+            action['place'] = env.action_space.spaces['place'].sample()
             obs, reward, done, info = env.step(action)
-            print(reward)
-        print("MISSION DONE")
+            if reward != 0:
+                print(reward)
+        print("MISSION DONE") 
 
     print("Demo Complete.")
 
@@ -54,6 +131,8 @@ if __name__ == '__main__':
                 step_env(sys.argv[2])
             else:
                 step_env()
+    elif len(sys.argv) > 0 and sys.argv[1] == 'test':
+        test_env()
     else:
         print("Testing data pipeline")
         step_data()
